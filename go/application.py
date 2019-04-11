a7ae5d287ee967381c4d05be30a5d3c6347240a4
Haowen Xu <haowen.xu@outlook.com>
2019-04-11 22:13:27 +0800

    added wiki

--- go/application.py
+++ go/application.py
@@ -1,7 +1,9 @@
 from sanic import Sanic
+from .wiki import wiki_bp
 from .zotero import zotero_bp
 
 __all__ = ['app']
 
 app = Sanic(__name__)
+app.blueprint(wiki_bp, url_prefix='/wiki')
 app.blueprint(zotero_bp, url_prefix='/zotero')
